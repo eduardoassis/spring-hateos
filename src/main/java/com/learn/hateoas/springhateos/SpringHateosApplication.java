7ceb0683219b51aa527aa8385b21b5e3e34cc0fd
Eduardo Assis da Silva <eduardouflassis@gmail.com>
2017-11-21 22:41:36 -0200

    Bootstrap of project spring-hateoas

---
+++ src/main/java/com/learn/hateoas/springhateos/SpringHateosApplication.java
@@ -0,0 +1,12 @@
+package com.learn.hateoas.springhateos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHateosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHateosApplication.class, args);
+	}
+}
---
+++ src/test/java/com/learn/hateoas/springhateos/SpringHateosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.learn.hateoas.springhateos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringHateosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
