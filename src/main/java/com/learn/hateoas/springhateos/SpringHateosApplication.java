8c6d4ad7a67a30ad72346637b361da83e70256b9
Eduardo Assis da Silva <eduardouflassis@gmail.com>
2017-11-22 23:43:55 -0200

    Add configuration

--- src/main/java/com/learn/hateoas/springhateos/SpringHateosApplication.java
+++ src/main/java/com/learn/hateoas/springhateos/SpringHateosApplication.java
@@ -2,8 +2,10 @@ package com.learn.hateoas.springhateos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.learn.hateoas.springhateos" })
 public class SpringHateosApplication {
 
 	public static void main(String[] args) {
